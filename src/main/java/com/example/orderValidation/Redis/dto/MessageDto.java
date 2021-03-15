c295c9d41f4de217ac1ea2fed6cea7d4dbc71d65
clem-kay <ckoomson75@gmail.com>
2021-03-15 09:13:41 +0000

    Added redis pub sub configuration for the order validation

---
+++ src/main/java/com/example/orderValidation/Redis/dto/MessageDto.java
@@ -0,0 +1,13 @@
+package com.example.orderValidation.Redis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageDto {
+    private String body;
+
+}
---
+++ src/test/java/com/example/orderValidation/OrderValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
